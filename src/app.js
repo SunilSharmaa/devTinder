c643d9348ef64ed03cfd7730ab99bc0140dc848d
Sunil Sharma <sunilsharma.workit@gmail.com>
2024-12-17 10:22:48 +0530

    play with routes and route extensions

--- src/app.js
+++ src/app.js
@@ -3,8 +3,16 @@ const express = require("express");
 const app = express();
 
 
+app.use("/test/2", (req, res)=> {
+    res.send("output from test----2 route")
+})
+
 app.use("/test", (req, res)=> {
     res.send("output from test route")
+})
+
+app.use("/hello/2", (req, res)=>{
+    res.send("output from hello-----2 route");
 })
 
 app.use("/hello", (req, res)=>{
