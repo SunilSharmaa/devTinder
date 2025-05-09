fe1caaf2ed8633694663a3af5e7a61cc31ea14c9
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-05-09 12:36:24 +0530

    add validation to User schema fields

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,13 +1,41 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName : String,
-    lastName : String,
-    age : Number,
-    gender : String,
-    emailId : String,
-    password : String
-})
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 12,
+  },
+
+  lastName: {
+    type: String,
+    maxLength: 15,
+  },
+
+  age: {
+    type: Number,
+    required: true,
+    min: 18,
+    max: 120,
+  },
+
+  gender: {
+    type: String,
+    validate: {
+      validator: (val) => val === "male" || val === "female" || val === "other",
+      message: "gender must be male, female or other",
+    },
+  },
+
+  emailId: {
+    type: String,
+    unique: true,
+    maxLength: 20,
+  },
+
+  password: String,
+});
 
 const User = mongoose.model("User", userSchema);
 
