5b3239fd5a3da29ef0b12753266d7d90843928ee
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-06-09 15:20:08 +0530

    refactor: add error handling to connection request API

--- src/router/requestRouter.js
+++ src/router/requestRouter.js
@@ -8,61 +8,58 @@ connectionRequestRouter.post(
   "/request/send/:status/:userId",
   authUser,
   async (req, res) => {
-    const loggedInUserId = req.user._id.toString();
-    const { status, userId } = req.params;
+    try {
+      const loggedInUserId = req.user._id.toString();
+      const { status, userId } = req.params;
 
-    const allowedStatus = ["interested", "ignored"];
-    const isStatusValid = allowedStatus.includes(status);
+      const allowedStatus = ["interested", "ignored"];
+      const isStatusValid = allowedStatus.includes(status);
 
-    if (!isStatusValid) {
-      return res.status(400).json({
-        message: "invalid status",
+      if (!isStatusValid) {
+        throw new Error("invalid status");}
+
+      const isUserExist =
+        (await User.countDocuments({
+          _id: userId,
+        })) > 0;
+
+      if (!isUserExist){
+        throw new Error("sending connection request to invalid user");
+      } 
+
+      const isConnectionRequestDuplicate =
+        (await ConnectionRequest.countDocuments({
+          $or: [
+            {
+              toUserId: loggedInUserId,
+              fromUserId: userId,
+            },
+            {
+              toUserId: userId,
+              fromUserId: loggedInUserId,
+            },
+          ],
+        })) > 0;
+
+      if (isConnectionRequestDuplicate) {
+          throw new Error("cannot send connection request again");
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        toUserId: userId,
+        fromUserId: loggedInUserId,
+        status: status,
+      });
+
+      await connectionRequest.save();
+      res.status(200).json({
+        message: "connection request send",
+      });
+    } catch (err) {
+      res.status(400).json({
+        error: err,
       });
     }
-
-    const isUserExist = (await User.countDocuments({
-        _id : userId,
-    })) > 0
-
-    if(!isUserExist) {
-        return res.status(400).json({
-            message : "sending connection request to invalid user",
-        })
-    }
-
-
-    const isConnectionRequestDuplicate =
-      (await ConnectionRequest.countDocuments({
-        $or: [
-          {
-            toUserId: loggedInUserId,
-            fromUserId: userId,
-          },
-          {
-            toUserId : userId,
-            fromUserId : loggedInUserId
-          }
-        ],
-
-      })) > 0;
-    
-
-    if (isConnectionRequestDuplicate) {
-      return res.status(400).json({
-        message: "cannot send connection request again",
-      });
-    }
-
-    const connectionRequest = new ConnectionRequest({
-      toUserId: userId,
-      fromUserId: loggedInUserId,
-      status: status,
-    });
-
-    await connectionRequest.save();
-    res.status(200).json({
-      message: "connection request send",
-    });
   }
 );
 
