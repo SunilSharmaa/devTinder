a8064cdfce5041fc8609d977dfb76cc6d0782516
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-06-16 16:25:19 +0530

    add : API to fetch all accepted user connections for the logged-in user

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -10,7 +10,7 @@ userRouter.get("/user/request/received", authUser, async(req, res) => {
         const loggedInUser = req.user;
     
         const connectionRequest = await ConnectionRequest.find({
-            toUserId : loggedInUser,
+            toUserId : loggedInUser._id,
             status : "interested"
         })
         .populate("fromUserId", "firstName lastName")
@@ -32,6 +32,55 @@ userRouter.get("/user/request/received", authUser, async(req, res) => {
         })
     }
 
+})
+
+userRouter.get("/user/connection", authUser, async(req, res)=> {
+
+    try{
+        const loggedInUserId = req.user._id.toString();
+
+        const userConnection = await ConnectionRequest.find({
+
+            $or : [
+                {
+                    fromUserId : loggedInUserId,
+                    status : "accepted"
+                },
+                {
+                    toUserId : loggedInUserId,
+                    status : "accepted"
+                }
+            ]
+        }
+         ).populate("fromUserId", "firstName lastName")
+         .populate("toUserId", "firstName lastName")
+
+         const userData = userConnection.map((key) => {
+
+            if(loggedInUserId === key.fromUserId._id.toString()) {
+                return key.toUserId
+            }
+
+            return key.fromUserId;
+         });
+
+         console.log(userConnection);
+
+         if(userConnection.length > 0) {
+            res.json({
+                data : userData
+            })
+         } else {
+            res.json({
+                data : "no connection found"
+            })
+         }
+
+    } catch (err) {
+        res.status(400).json({
+            error : err.message
+        })
+    }
 })
 
 module.exports = userRouter;
