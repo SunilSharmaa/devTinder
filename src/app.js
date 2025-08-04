0753515bdb81cd0723f29a1f9ddc8de0bff10ebd
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-08-04 09:33:43 +0530

    feat: setup Express app with routing, CORS, and MongoDB connection

--- src/app.js
+++ src/app.js
@@ -7,25 +7,31 @@ const authUser = require("./validation/authUser");
 const app = express();
 const authRouter = require("./router/authRouter");
 const profileRouter = require("./router/profileRouter");
-const connectionRequestRouter = require("./router/requestRouter")
+const connectionRequestRouter = require("./router/requestRouter");
 const userRouter = require("./router/userRouter");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", connectionRequestRouter);
 app.use("/", userRouter);
 
-
-app.get("/profile",  authUser,  (req, res) => {
+app.get("/profile", authUser, (req, res) => {
   res.send(req.user);
 });
 
 app.post("/sendConnection", authUser, (req, res) => {
-  res.send("connected successfully: " + req.user)
-})
+  res.send("connected successfully: " + req.user);
+});
 
 //get user api
 app.get("/user", async (req, res) => {
